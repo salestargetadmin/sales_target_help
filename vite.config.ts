4b109f5604c8cb0cf3d2f72c73a2cec093a0f901
Gaurav <gaurav.p@247compliance.com>
2025-03-28 14:09:22 +0530

    Update deployment workflow and Vite configuration to use 'dist' directory for production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: "index.js",
-        assetFileNames: "index.css",
-      },
-    },
-  },
 });
