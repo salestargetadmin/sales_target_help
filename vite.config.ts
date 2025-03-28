be5400a8c3ed936e7baa7fe88fb2ce07fa3b7857
Gaurav <gaurav.p@247compliance.com>
2025-03-28 17:25:24 +0530

    Update Vite build configuration to output to 'build' directory and adjust deployment workflow to sync with S3

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js",
+        assetFileNames: "index.css",
+      },
+    },
+  },
 });
